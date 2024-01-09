7519b21624bf3c7946a4182ff524bec19df1abe9
Dr. Dillinger <peter.dillinger@srh.de>
2024-01-09 11:55:36 +0100

    Modell der Firma realisiert!

--- src/Firma.java
+++ src/Firma.java
@@ -1,2 +1,11 @@
 public class Firma {
+    public String waren;
+
+    public Firma(String waren) {
+        this.waren = waren;
+    }
+
+    public void produzieren() {
+        System.out.println("Die Firma produziert " + this.waren);
+    }
 }
