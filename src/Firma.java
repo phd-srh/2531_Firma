54dd6055f48a8548d0fa5d871cf4cc0110182e34
Dr. Dillinger <peter.dillinger@srh.de>
2024-01-16 09:51:40 +0100

    Konzept der Kapselung (information hiding) umgesetzt!

--- src/Firma.java
+++ src/Firma.java
@@ -1,7 +1,20 @@
 public class Firma {
-    public String waren;
+    private String waren;
 
     public Firma(String waren) {
+        this.waren = waren;
+    }
+
+    // standard Getter für das Attribut "waren"
+    public String getWaren() {
+        return this.waren;
+    }
+
+    // standard Setter für das Attribut "waren"
+    public void setWaren(String waren) {
+        if (waren.equals("Falschgeld") || waren.equals("Waffen") ||
+            waren.equals("Drogen"))
+            return; // <- early exit, wir tun das folgende nicht !
         this.waren = waren;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Firma bäckerei = new Firma("Brötchen");
+        // bäckerei.waren = "Falschgeld"; <- das geht nicht mehr! weil "private"
+        System.out.println("Was produziert die Bäckerei? " + bäckerei.getWaren());
+        bäckerei.setWaren("Falschgeld");
         bäckerei.produzieren();
     }
 }
